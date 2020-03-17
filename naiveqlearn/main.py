b3de104b5ef2fbd949a5f52ef3724e2e96ed363f
Sebastian Häni <haeni.sebastian@gmail.com>
2020-03-17 23:46:21 +0100

    naive q learning main.py refactor

--- naiveqlearn/main.py
+++ naiveqlearn/main.py
@@ -7,13 +7,17 @@ from naiveqlearn.policy import Policy
 from naiveqlearn.train import train
 from naiveqlearn.wrapper import DiscreteLunarEnv
 
-env = gym.make('LunarLander-v2')
-env = Monitor(env, '/tmp/qlearn', force=True)
-env = DiscreteLunarEnv(env)
-policy = Policy(env)
 
-train(Policy(env), Config())
+def main():
+    env = gym.make('LunarLander-v2')
+    env = Monitor(env, '/tmp/qlearn', force=True)
+    env = DiscreteLunarEnv(env)
 
-env.close()
-plot_from_monitor_results('/tmp/qlearn', window=50)
+    train(Policy(env), Config())
 
+    env.close()
+    plot_from_monitor_results('/tmp/qlearn', window=50)
+
+
+if __name__ == "__main__":
+    main()
